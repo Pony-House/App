2e82fae4b00fbd2373ae277a6387db796dcd0c76
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-11-26 23:18:56 -0300

    common.jsx --> user events script updated.

--- src/app/organisms/room/common.jsx
+++ src/app/organisms/room/common.jsx
@@ -147,122 +147,312 @@ function getUsersActionJsx(roomId, userIds, actionStr) {
   );
 }
 
-function parseTimelineChange(mEvent) {
+export function getUserEventType(mEvent) {
   const tJSXMsgs = getTimelineJSXMessages();
-  const makeReturnObj = (variant, content) => ({
-    variant,
-    content,
-  });
-
-  const appearanceSettings = getAppearance();
-  const mx = initMatrix.matrixClient;
-
-  const eventMoment = moment(mEvent.getDate());
-  const eventId = mEvent.getId();
 
   const type = mEvent.getType();
-  const date = eventMoment.isValid() ? eventMoment.valueOf() || 0 : 0;
   const content = mEvent.getContent();
   const prevContent = mEvent.getPrevContent();
+
   const sender = mEvent.getSender();
   const senderName = getUsername(sender);
-  const userName = getUsername(mEvent.getStateKey());
 
   if (type !== 'm.room.pinned_events') {
     switch (content.membership) {
+      // Invite
       case 'invite':
-        return makeReturnObj('invite', tJSXMsgs.invite(date, senderName, userName, eventId));
+        return 'invite';
+      // Ban
       case 'ban':
-        return makeReturnObj(
-          'leave',
-          tJSXMsgs.ban(date, senderName, userName, content.reason, eventId),
-        );
+        return 'ban';
 
+      // User events
       case 'join':
         if (prevContent.membership === 'join') {
           if (content.displayname !== prevContent.displayname) {
-            if (typeof content.displayname === 'undefined')
-              return makeReturnObj(
-                date,
-                'avatar',
-                tJSXMsgs.nameRemoved(date, sender, prevContent.displayname, eventId),
-              );
-            if (typeof prevContent.displayname === 'undefined')
-              return makeReturnObj(
-                date,
-                'avatar',
-                tJSXMsgs.nameSets(date, sender, content.displayname, eventId),
-              );
-            return makeReturnObj(
-              'avatar',
-              tJSXMsgs.nameChanged(date, prevContent.displayname, content.displayname, eventId),
-            );
+            // Name removed
+            if (typeof content.displayname === 'undefined') return 'nameRemoved';
+            // New name
+            if (typeof prevContent.displayname === 'undefined') return 'nameSets';
+            // Name changed
+            return 'nameChanged';
           }
           if (content.avatar_url !== prevContent.avatar_url) {
-            if (typeof content.avatar_url === 'undefined')
-              return makeReturnObj(
-                'avatar',
-                tJSXMsgs.avatarRemoved(date, content.displayname, eventId),
-              );
-            if (typeof prevContent.avatar_url === 'undefined')
-              return makeReturnObj(
-                'avatar',
-                tJSXMsgs.avatarSets(date, content.displayname, eventId),
-              );
-            return makeReturnObj(
-              'avatar',
-              tJSXMsgs.avatarChanged(date, content.displayname, eventId),
-            );
+            // Avatar removed
+            if (typeof content.avatar_url === 'undefined') return 'avatarRemoved';
+            // Avatar set
+            if (typeof prevContent.avatar_url === 'undefined') return 'avatarSets';
+            // Avatar changed
+            return 'avatarChanged';
           }
+          // What?
           return null;
         }
-        return makeReturnObj('join', tJSXMsgs.join(eventMoment, senderName, eventId));
+        // New join event
+        return 'join';
 
+      // Leave Events
       case 'leave':
+        // Made by user
         if (sender === mEvent.getStateKey()) {
           switch (prevContent.membership) {
+            // Reject Invite
             case 'invite':
-              return makeReturnObj(
-                'invite-cancel',
-                tJSXMsgs.rejectInvite(date, senderName, eventId),
-              );
+              return 'rejectInvite';
+            // Leave Message
             default:
-              return makeReturnObj(
-                'leave',
-                tJSXMsgs.leave(eventMoment, senderName, content.reason, eventId),
-              );
+              return 'leave';
           }
         }
+        // Other
         switch (prevContent.membership) {
+          // Cancel Invite
           case 'invite':
-            return makeReturnObj(
-              'invite-cancel',
-              tJSXMsgs.cancelInvite(date, senderName, userName, eventId),
-            );
+            return 'cancelInvite';
+          // Ban?
           case 'ban':
-            return makeReturnObj('other', tJSXMsgs.unban(date, senderName, userName, eventId));
+            return 'unban';
           // sender is not target and made the target leave,
           // if not from invite/ban then this is a kick
           default:
-            return makeReturnObj(
-              'leave',
-              tJSXMsgs.kick(date, senderName, userName, content.reason, eventId),
-            );
+            return 'kick';
         }
 
+      // Nothing
       default:
         return null;
     }
   }
+}
+
+// User Events
+const makeReturnObj = (variant, content) => ({
+  variant,
+  content,
+});
+
+export const userEventsMessage = {
+  // Invite
+  invite: (mEvent) => {
+    const eventMoment = moment(mEvent.getDate());
+    const date = eventMoment.isValid() ? eventMoment.valueOf() || 0 : 0;
+    const eventId = mEvent.getId();
+    const sender = mEvent.getSender();
+    const senderName = getUsername(sender);
+    const userName = getUsername(mEvent.getStateKey());
+    return makeReturnObj(
+      'invite',
+      getTimelineJSXMessages().invite(date, senderName, userName, eventId),
+    );
+  },
+
+  // Ban
+  ban: (mEvent) => {
+    const eventMoment = moment(mEvent.getDate());
+    const date = eventMoment.isValid() ? eventMoment.valueOf() || 0 : 0;
+    const eventId = mEvent.getId();
+    const content = mEvent.getContent();
+    const sender = mEvent.getSender();
+    const senderName = getUsername(sender);
+    const userName = getUsername(mEvent.getStateKey());
+    return makeReturnObj(
+      'leave',
+      getTimelineJSXMessages().ban(date, senderName, userName, content.reason, eventId),
+    );
+  },
+
+  // Name removed
+  nameRemoved: (mEvent) => {
+    const eventMoment = moment(mEvent.getDate());
+    const date = eventMoment.isValid() ? eventMoment.valueOf() || 0 : 0;
+    const eventId = mEvent.getId();
+    const sender = mEvent.getSender();
+    const prevContent = mEvent.getPrevContent();
+    return makeReturnObj(
+      date,
+      'avatar',
+      getTimelineJSXMessages().nameRemoved(date, sender, prevContent.displayname, eventId),
+    );
+  },
+
+  // New name
+  nameSets: (mEvent) => {
+    const eventMoment = moment(mEvent.getDate());
+    const date = eventMoment.isValid() ? eventMoment.valueOf() || 0 : 0;
+    const eventId = mEvent.getId();
+    const content = mEvent.getContent();
+    const sender = mEvent.getSender();
+    return makeReturnObj(
+      date,
+      'avatar',
+      getTimelineJSXMessages().nameSets(date, sender, content.displayname, eventId),
+    );
+  },
+
+  // Name changed
+  nameChanged: (mEvent) => {
+    const eventMoment = moment(mEvent.getDate());
+    const date = eventMoment.isValid() ? eventMoment.valueOf() || 0 : 0;
+    const eventId = mEvent.getId();
+    const content = mEvent.getContent();
+    const prevContent = mEvent.getPrevContent();
+    return makeReturnObj(
+      'avatar',
+      getTimelineJSXMessages().nameChanged(
+        date,
+        prevContent.displayname,
+        content.displayname,
+        eventId,
+      ),
+    );
+  },
+
+  // Avatar removed
+  avatarRemoved: (mEvent) => {
+    const eventMoment = moment(mEvent.getDate());
+    const date = eventMoment.isValid() ? eventMoment.valueOf() || 0 : 0;
+    const eventId = mEvent.getId();
+    const content = mEvent.getContent();
+    return makeReturnObj(
+      'avatar',
+      getTimelineJSXMessages().avatarRemoved(date, content.displayname, eventId),
+    );
+  },
+
+  // Avatar set
+  avatarSets: (mEvent) => {
+    const eventMoment = moment(mEvent.getDate());
+    const date = eventMoment.isValid() ? eventMoment.valueOf() || 0 : 0;
+    const eventId = mEvent.getId();
+    const content = mEvent.getContent();
+    return makeReturnObj(
+      'avatar',
+      getTimelineJSXMessages().avatarSets(date, content.displayname, eventId),
+    );
+  },
+
+  // Avatar changed
+  avatarChanged: (mEvent) => {
+    const eventMoment = moment(mEvent.getDate());
+    const date = eventMoment.isValid() ? eventMoment.valueOf() || 0 : 0;
+    const eventId = mEvent.getId();
+    const content = mEvent.getContent();
+    return makeReturnObj(
+      'avatar',
+      getTimelineJSXMessages().avatarChanged(date, content.displayname, eventId),
+    );
+  },
+
+  // New join event
+  join: (mEvent) => {
+    const eventMoment = moment(mEvent.getDate());
+    const date = eventMoment.isValid() ? eventMoment.valueOf() || 0 : 0;
+    const eventId = mEvent.getId();
+    const sender = mEvent.getSender();
+    const senderName = getUsername(sender);
+    return makeReturnObj('join', getTimelineJSXMessages().join(eventMoment, senderName, eventId));
+  },
+
+  // Reject Invite
+  rejectInvite: (mEvent) => {
+    const eventMoment = moment(mEvent.getDate());
+    const date = eventMoment.isValid() ? eventMoment.valueOf() || 0 : 0;
+    const eventId = mEvent.getId();
+    const sender = mEvent.getSender();
+    const senderName = getUsername(sender);
+    return makeReturnObj(
+      'invite-cancel',
+      getTimelineJSXMessages().rejectInvite(date, senderName, eventId),
+    );
+  },
+
+  // Leave Message
+  leave: (mEvent) => {
+    const eventMoment = moment(mEvent.getDate());
+    const eventId = mEvent.getId();
+    const content = mEvent.getContent();
+    const sender = mEvent.getSender();
+    const senderName = getUsername(sender);
+    return makeReturnObj(
+      'leave',
+      getTimelineJSXMessages().leave(eventMoment, senderName, content.reason, eventId),
+    );
+  },
+
+  // Cancel Invite
+  cancelInvite: (mEvent) => {
+    const eventMoment = moment(mEvent.getDate());
+    const date = eventMoment.isValid() ? eventMoment.valueOf() || 0 : 0;
+    const eventId = mEvent.getId();
+    const sender = mEvent.getSender();
+    const senderName = getUsername(sender);
+    const userName = getUsername(mEvent.getStateKey());
+    return makeReturnObj(
+      'invite-cancel',
+      getTimelineJSXMessages().cancelInvite(date, senderName, userName, eventId),
+    );
+  },
+
+  // Uban
+  unban: (mEvent) => {
+    const eventMoment = moment(mEvent.getDate());
+    const date = eventMoment.isValid() ? eventMoment.valueOf() || 0 : 0;
+    const eventId = mEvent.getId();
+    const sender = mEvent.getSender();
+    const senderName = getUsername(sender);
+    const userName = getUsername(mEvent.getStateKey());
+    return makeReturnObj(
+      'other',
+      getTimelineJSXMessages().unban(date, senderName, userName, eventId),
+    );
+  },
+
+  // Kick
+  kick: (mEvent) => {
+    const eventMoment = moment(mEvent.getDate());
+    const date = eventMoment.isValid() ? eventMoment.valueOf() || 0 : 0;
+    const eventId = mEvent.getId();
+    const content = mEvent.getContent();
+    const sender = mEvent.getSender();
+    const senderName = getUsername(sender);
+    const userName = getUsername(mEvent.getStateKey());
+    return makeReturnObj(
+      'leave',
+      getTimelineJSXMessages().kick(date, senderName, userName, content.reason, eventId),
+    );
+  },
+};
+
+function parseTimelineChange(mEvent) {
+  const appearanceSettings = getAppearance();
+  const mx = initMatrix.matrixClient;
+  const type = mEvent.getType();
+
+  if (type !== 'm.room.pinned_events') {
+    const userEventType = getUserEventType(mEvent);
+    if (typeof userEventsMessage[userEventType] === 'function')
+      return userEventsMessage[userEventType](mEvent);
+    else return null;
+  }
 
   // Pin Messages
   if (typeof mEvent.getStateKey() === 'string') {
+    const eventMoment = moment(mEvent.getDate());
+    const date = eventMoment.isValid() ? eventMoment.valueOf() || 0 : 0;
+
+    const eventId = mEvent.getId();
+    const content = mEvent.getContent();
+
+    const sender = mEvent.getSender();
+    const senderName = getUsername(sender);
+
     const comparedPinMessages = comparePinEvents(content, mEvent.getPrevContent());
 
     if (
       (comparedPinMessages.added.length > 0 && !appearanceSettings.hidePinMessageEvents) ||
       (comparedPinMessages.removed.length > 0 && !appearanceSettings.hideUnpinMessageEvents)
     ) {
+      const tJSXMsgs = getTimelineJSXMessages();
       return makeReturnObj(
         `pinned-events-${comparedPinMessages.added.length > 0 ? 'added' : 'removed'}`,
         tJSXMsgs.pinnedEvents(
